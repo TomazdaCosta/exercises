0edaef5da7f1405773b91ea5011f61b9377ff255
TomazdaCosta <ftomazc7@gmail.com>
2026-01-30 21:03:28 -0300

    Criado o componente de Nav.

---
+++ react-with-typescript/exercicio-01/src/components/Nav.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+      <ol>
+        <li>Tasks</li>
+        <li>Usuário</li>
+      </ol>
+    </div>
+  )
+}
+
+export default Nav
