87b34e878355a278ab05e3b8f1745f00a697f8ac
TomazdaCosta <ftomazc7@gmail.com>
2026-02-20 23:07:53 -0300

    Criado a função de aparecer o input para alterar o título da task.

--- react-with-typescript/exercicio-04/src/components/ItemTask.tsx
+++ react-with-typescript/exercicio-04/src/components/ItemTask.tsx
@@ -1,33 +1,53 @@
+import React from "react"
 import type { ITaskItem } from "../types/types"
 
 const ItemTask = ({id, title, complete, completeTask, removeTask}: ITaskItem) => {
+  const [newTaskTitle, setNewTaskTitle] = React.useState('')
+  const [changeTitle, setChangeTitle] = React.useState(false)
   const completeStatus = complete ? 'green' : 'red'
 
   return (
-    <div key={id} style={{display: 'flex', gap: '4px', alignItems: 'center'}}>
+    <div key={id}>
+      <div style={{display: 'flex', gap: '4px', alignItems: 'center'}}>
+        <span style={{width: '8px', height: '8px', backgroundColor: completeStatus, display: 'inline-block', borderRadius: '50%'}}></span>
 
-      <span style={{width: '8px', height: '8px', backgroundColor: completeStatus, display: 'inline-block', borderRadius: '50%'}}></span>
+        <p>{title}</p>
 
-      <p>{title}</p>
+        {complete ? null :
+          <div style={{display: 'flex', gap: '4px'}}>
+            <button
+              onClick={() => setChangeTitle(currentValue => !currentValue)}
+            >Alterar</button>
+            
+            <button
+              onClick={() => completeTask(id)}
+            >
+              Completar
+            </button>
 
-      {complete ? null :
-        <div style={{display: 'flex', gap: '4px'}}>
-          <button>Alterar</button>
-          
-          <button
-            onClick={() => completeTask(id)}
-          >
-            Completar
-          </button>
-
-          <button
-            onClick={() => removeTask(id)}
-          >
-            Excluir
-          </button>
+            <button
+              onClick={() => removeTask(id)}
+            >
+              Excluir
+            </button>
+          </div>
+        }
         </div>
-      }
-      
+
+        <div>
+          {changeTitle &&
+            <div>
+              <input
+                type="text"
+                placeholder="novo título"
+                value={newTaskTitle}
+                onChange={({ target }) => setNewTaskTitle(target.value)}
+              />
+
+              <button>Salvar</button>
+            </div>
+          }
+        </div>
     </div>
   )
 }
